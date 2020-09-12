fc58711d1795d4364a0393cbd5468416fcc7e024
chaesono <codnjswls12@naver.com>
2020-09-12 14:05:55 +0900

    Updates

---
+++ precache-manifest.c148b6a06ce6c517ccee8f7bc2bad3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699ddcbf66d9a69dd4db64f97546cdea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "661a2bf711ff2f4d9872",
+    "url": "/monsters-rolodex/static/css/main.4b656e8b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661a2bf711ff2f4d9872",
+    "url": "/monsters-rolodex/static/js/main.d52b23a8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
